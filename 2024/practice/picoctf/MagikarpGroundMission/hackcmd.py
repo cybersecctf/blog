6393c2872d32d62763088a683963e576ce764795
phantom1ss <phantomss.team@gmail.com>
2024-04-03 12:01:48 -0400

    2024.3.4.6

--- 2024/practice/picoctf/MagikarpGroundMission/hackcmd.py
+++ 2024/practice/picoctf/MagikarpGroundMission/hackcmd.py
@@ -1,23 +1,3 @@
-
-<!DOCTYPE html>
-<html>
-
-<body>
-    <h1>Magikarp Ground Mission- picoctf2021</h1>
-
-    <h2>Challenge Description</h2>
-AUTHOR: MADSTACKS
-
-Description
-I've hidden a flag in this file. Can you find it? 
-    <a href="https://mercury.picoctf.net/static/9a7436948cc502e9cacf5bc84d2cccb5/Forensics%20is%20fun.pptm">Forensics is fun.pptm</a>
- 
-    <h2>Solution Approach</h2>
-    <p>Here are the steps we took to solve the challenge:</p>
-    <ol>
-         remove space in name and convert it   to <a href="https://mercury.picoctf.net/static/9a7436948cc502e9cacf5bc84d2cccb5/Forensicsisfun.pptm">Forensics is fun.pptm</a>
-    and see is hidden file use command cat ppt/slideMasters/hidden and get text is like base64 but not complete mode so use this code for get flag from this
-<pre>
 import base64
 
 def test_base64(string):
@@ -58,18 +38,3 @@ if valid_base64:
     print("Valid base64 string and text:", valid_base64, base64.b64decode(valid_base64))
 else:
     print("Not a valid base64 string.")
-
-</pre> 
-after run can get flag
-    </ol>
-<br>
-    <h2>Flag</h2>
-    <p class="flag">picoCTF{D1d_u_kn0w_ppts_r_z1p5}
-</p>
-
-    <h2>Conclusion</h2>
-    <p>this is a very   easy chanllenge for search on pptm and decrypt b</p>
-</body>
-</html>
-
-
