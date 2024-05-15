f69e2f1456b448221e5bc3db85165aa7b8efef10
cybersecctf <phantomss.team@gmail.com>
2024-05-15 03:51:33 -0400

    2024.15.5.1

--- 2024/practice/cryptohack/BytesandBigIntegers/hackcmd.py
+++ 2024/practice/cryptohack/BytesandBigIntegers/hackcmd.py
@@ -1,38 +1,3 @@
-
-<!DOCTYPE html>
-<html>
-
-<body>
-    <h1>Bytes and Big Integers- cryptohack</h1>
-
-    <h2>Challenge Description</h2>
-    <p> Cryptosystems like RSA works on numbers, but messages are made up of characters. How should we convert our messages into numbers so that mathematical operations can be applied?
-
-The most common way is to take the ordinal bytes of the message, convert them into hexadecimal, and concatenate. This can be interpreted as a base-16/hexadecimal number, and also represented in base-10/decimal.
-
-To illustrate:
-
-message: HELLO
-ascii bytes: [72, 69, 76, 76, 79]
-hex bytes: [0x48, 0x45, 0x4c, 0x4c, 0x4f]
-base-16: 0x48454c4c4f
-base-10: 310400273487
-
- Python's PyCryptodome library implements this with the methods bytes_to_long() and long_to_bytes(). You will first have to install PyCryptodome and import it with from Crypto.Util.number import *. For more details check the FAQ.
-
-
-Convert the following integer back into a message:
-
-11515195063862318899931685488813747395775516287289682636499965282714637259206269
- 
-</p>
- 
-    <h2>Solution Approach</h2>
-    <p>Here are the steps we took to solve the challenge:</p>
-    <ol>
-        <li> steps1 </li>
-       we can use this script to decode/encode on asci/long/nase64 and get message
-<pre>
 import base64, sys
 from Crypto.Util.number import long_to_bytes, bytes_to_long
 
@@ -78,17 +43,3 @@ else:  # encode
         print(bytes_to_long(input_value.encode('utf-8')))
     else:  # base64
         print(base64.b64encode(input_value.encode('utf-8')).decode('utf-8'))
-
-</pre>
-    and print flag
-    </ol>
-<br>
-    <h2>Flag</h2>
-    <p class="flag">crypto{3nc0d1n6_4ll_7h3_w4y_d0wn}
-</p>
-
-    <h2>Conclusion</h2>
-    <p>this is a very   easy chanllenge for decode encodeon base64 asci long</p>
-</body>
-</html>
-
