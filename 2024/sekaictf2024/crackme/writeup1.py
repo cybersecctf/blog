18572f7f2a54044389a028225218c21d5dd1711c
cybersecctf <phantomss.team@gmail.com>
2024-09-02 19:57:49 +0330

    2024.2.9.1

---
+++ 2024/sekaictf2024/crackme/writeup1.py
@@ -0,0 +1,8 @@
+
+import sys
+sys.path.append('/home/solup/Desktop/blog')  # This is an absolute path
+import blog
+def solve(path="app.apk",search="flag"):
+ return f"open jadax and open{path} in it and sarch {search} in it"
+if __name__ == "__main__" :
+ print(solve())
