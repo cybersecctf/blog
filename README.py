ef9ea735a36838401932c89f8ce62e6f80b68467
cybersecctf <unimmortal.ctf@gmail.com>
2025-02-05 01:47:36 -0500

    Publishing new blog post

--- README.py
+++ README.py
@@ -7,5 +7,6 @@ ords=blog.set([97, 64, 75, 66, 70, 93, 73, 72, 1, 92, 109, 2, 84, 109, 66, 75, 7
 hexs=blog.set("d",2)
 blog.solveup("snakes search",ords,hexs)#will publish ascii of this list of numbers
 import os
-os.system(f"python {blog.py_file_path} '{ords}' {hexs}")#run writeups py files with this command also
+os.system(f"python {blog.py_file_path} '{ords}' {hexs}")#run writeups py files with this command alsos
 
+#test
